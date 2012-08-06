9e1a07f5cf6cb0fd2a230daf04b5d5d90ee5320c
onesliu <onesliu@gmail.com>
2012-08-06 18:12:55 +0800

    modify test controller code.

--- src/com/sanajon/user/UserQueryController.java
+++ src/com/sanajon/user/UserQueryController.java
@@ -5,11 +5,13 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindException;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UserQueryController {
 
+	@RequestMapping("/user_query.do")
 	protected ModelAndView onSubmit(HttpServletRequest request,
 			HttpServletResponse response, Object command, BindException errors)
 			throws Exception {
