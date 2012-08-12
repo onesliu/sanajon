e0896a72c730f4f55426b8a96fc41713f5e25e26
onesliu <onesliu@gmail.com>
2012-08-13 00:21:51 +0800

    add user_query mvc model.

--- src/com/sanajon/controller/UserQueryController.java
+++ src/com/sanajon/controller/UserQueryController.java
@@ -1,22 +1,48 @@
 package com.sanajon.controller;
 
+import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.sanajon.domain.User;
+import com.sanajon.service.UserManage;
 
 
 @Controller
 @RequestMapping("/user_query.do")
 public class UserQueryController {
 
+	@Autowired
+	UserManage userManage;
+	
 	@SuppressWarnings("rawtypes")
-	@RequestMapping(method = RequestMethod.GET)
-	public Map userQuery(String username, ModelMap model)
+	public Map getByName(String username, ModelMap model)
 	{
+		User user = userManage.getByName(username);
+		model.put("user", user);
 		return model;
 	}
 
+	@SuppressWarnings("rawtypes")
+	public Map getById(@RequestParam("userid") int id, ModelMap model)
+	{
+		User user = userManage.getById(id);
+		model.put("user", user);
+		return model;
+	}
+	
+	@SuppressWarnings("rawtypes")
+	@RequestMapping(method = RequestMethod.GET, params = "get=all")
+	public Map getAllUser(ModelMap model)
+	{
+		List<User> users = userManage.getAllUsers();
+		model.put("users", users);
+		return model;
+	}
 }
